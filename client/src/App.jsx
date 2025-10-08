5eb5c4d3c821165cba3af1daa04d35d68439efe6
Ramy <ramy@Kurama.localdomain>
2025-10-08 07:44:54 -0400

    fix: fixed route handling for home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Landing from "./pages/Landing/Landing";
 import Header from "./components/Header/Header";
 import { ThemeProvider } from "./components/Theme/ThemeProvider";
 import { FavoritesProvider } from "./components/Favorites/FavoritesProvider";
+import Home from "./pages/Home/Home";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                         <Header />
                         <Routes>
                             <Route path="/" element={<Landing />} />
+                            <Route path="/home" element={<Home />} />
                         </Routes>
                     </FavoritesProvider>
                 </ThemeProvider>
