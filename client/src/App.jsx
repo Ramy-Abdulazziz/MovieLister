646e0dc8e961b821babb2d6a4882773eeecbf44c
Ramy <ramy@Kurama.localdomain>
2025-10-08 06:57:00 -0400

    fix: added ThemeProvider wrapper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Landing from "./pages/Landing/Landing";
 import Header from "./components/Header/Header";
+import { ThemeProvider } from "./components/Theme/ThemeProvider";
 
 function App() {
     return (
         <>
             <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Landing />} />
-                </Routes>
+                <ThemeProvider>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Landing />} />
+                    </Routes>
+                </ThemeProvider>
             </BrowserRouter>
         </>
     );
