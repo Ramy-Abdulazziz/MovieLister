63716e557287e37eca10b5d121f07a12486c07ab
Ramy <ramy@Kurama.localdomain>
2025-10-08 06:37:58 -0400

    added header for all pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Landing from "./pages/Landing/Landing";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
         <>
             <BrowserRouter>
+                <Header />
                 <Routes>
                     <Route path="/" element={<Landing />} />
                 </Routes>
