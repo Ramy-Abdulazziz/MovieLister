eaae53f129151a0c33a7bc53864d35f4a2f0ed38
Ramy <ramy@Kurama.localdomain>
2025-10-08 07:21:34 -0400

    feat: favorites access
    created custom hook to access favorites via favorite context. Created home page structure for home and containers to hold single movies
    and multiple movies on page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Landing from "./pages/Landing/Landing";
 import Header from "./components/Header/Header";
 import { ThemeProvider } from "./components/Theme/ThemeProvider";
+import { FavoritesProvider } from "./components/Favorites/FavoritesProvider";
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <ThemeProvider>
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<Landing />} />
-                    </Routes>
+                    <FavoritesProvider>
+                        <Header />
+                        <Routes>
+                            <Route path="/" element={<Landing />} />
+                        </Routes>
+                    </FavoritesProvider>
                 </ThemeProvider>
             </BrowserRouter>
         </>
