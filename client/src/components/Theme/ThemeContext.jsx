00ab7b51c5c1e1f1b0fc50558dfb941c592220bd
Ramy <ramy@Kurama.localdomain>
2025-10-08 07:00:55 -0400

    fix: fixed issue with switched context and provider

--- client/src/components/Theme/ThemeContext.jsx
+++ client/src/components/Theme/ThemeContext.jsx
@@ -1,29 +1,3 @@
-import { useState, useEffect } from "react";
-import { ThemeContext } from "./ThemeContext";
+import { createContext } from "react";
 
-export const ThemeProvider = ({ children }) => {
-    const getInitialTheme = () => {
-        const savedTheme = localStorage.getItem("theme");
-        return savedTheme || "dark";
-    };
-    const [theme, setTheme] = useState(getInitialTheme);
-    useEffect(() => {
-        const savedTheme = localStorage.getItem("theme");
-        if (savedTheme) setTheme(savedTheme);
-    }, []);
-
-    useEffect(() => {
-        document.body.className = theme;
-        localStorage.setItem("theme", theme);
-    }, [theme]);
-
-    const toggleTheme = () => {
-        setTheme((currTheme) => (currTheme === "light" ? "dark" : "light"));
-    };
-
-    return (
-        <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    );
-};
+export const ThemeContext = createContext(null);
--- client/src/components/Theme/ThemeProvider.jsx
+++ client/src/components/Theme/ThemeProvider.jsx
@@ -1,3 +1,29 @@
-import { createContext } from "react";
+import { useState, useEffect } from "react";
+import { ThemeContext } from "./ThemeContext";
 
-export const ThemeContext = createContext(null);
+export const ThemeProvider = ({ children }) => {
+    const getInitialTheme = () => {
+        const savedTheme = localStorage.getItem("theme");
+        return savedTheme || "dark";
+    };
+    const [theme, setTheme] = useState(getInitialTheme);
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme");
+        if (savedTheme) setTheme(savedTheme);
+    }, []);
+
+    useEffect(() => {
+        document.body.className = theme;
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((currTheme) => (currTheme === "light" ? "dark" : "light"));
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
